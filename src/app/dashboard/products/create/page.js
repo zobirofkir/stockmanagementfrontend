d62504f0b7ed37075aab9c54435938a3a191de03
zobirofkir <zobirofkir19@gmail.com>
2024-12-15 16:13:37 +0100

    create product compponent

--- src/app/dashboard/products/create/page.js
+++ src/app/dashboard/products/create/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import CreateProductComponent from '@/components/products/CreateProductComponent'
+import CreateProductComponent from '@/components/products/create/CreateProductComponent';
 import React from 'react'
 
 const page = () => {
