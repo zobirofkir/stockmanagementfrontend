01f0442460fb0bf5b767c3abfbb72bd0506fab1b
zobirofkir <zobirofkir19@gmail.com>
2024-12-16 19:37:46 +0100

    create update supplier component

---
+++ src/app/dashboard/suppliers/update/page.js
@@ -0,0 +1,15 @@
+import UpdateSupplierComponent from '@/components/suppliers/UpdateSupplierComponent'
+import ProtectedRoute from '@/protected/protectedRoute'
+import React from 'react'
+
+const page = () => {
+  return (
+    <ProtectedRoute>
+      <div>
+        <UpdateSupplierComponent />
+      </div>
+    </ProtectedRoute>
+  )
+}
+
+export default page
---
+++ src/components/suppliers/UpdateSupplierComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateSupplierComponent = () => {
+  return (
+    <div>UpdateSupplierComponent</div>
+  )
+}
+
+export default UpdateSupplierComponent
